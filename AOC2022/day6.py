85728b7e2694af0f7947a3a7f3f6c50803cf3456
Silas Maughan <silas.maughan@gmail.com>
2022-12-06 17:54:30 +0100

    Day 6

--- AOC2022/day6.py
+++ AOC2022/day6.py
@@ -1,4 +1,18 @@
-#txt = open("input.txt", "r")
-f = open("input.txt","r")
-for x in f:
-    print(x)
+# Part 1 & 2 just change num of distinct
+import collections
+with(open("/Users/silasmaughan/CodingProjects/AOC/AOC2022/input.txt", "r") as txt):
+    line = list(txt.read())
+    found = False
+    i = 0
+    distinct = 14
+    while (found == False):
+        temp = []
+        for x in range(distinct):
+            temp.append(line[i + x])
+        temp = [item for item, count in collections.Counter(temp).items() if count > 1] 
+        if len(temp) == 0:
+            found = True
+            print(i + distinct)
+            break
+        else:
+            i += 1
